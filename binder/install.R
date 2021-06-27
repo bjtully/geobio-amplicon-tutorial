9bd697904764a1dc958707bbef10ba845e7f1bd7
Benjamin Tully <btully@Benjamins-MacBook-Pro.local>
2021-06-27 13:38:02 -0700

    Initial files for amplicon analysis

--- binder/install.R
+++ binder/install.R
@@ -1,13 +1,23 @@
-### install regular packages
+install.packages("vegan")
+install.packages("dendextend")
+install.packages("tidyr")
+install.packages("viridis")
+install.packages("reshape")
 
-install.packages("reticulate") # python support in RMarkdown
-install.packages("ggplot2") # for plotting
-install.packages(c("rmarkdown", "caTools", "bitops")) # for knitting
 
-### install bioconductor packages
-# install.packages("BiocManager")
-# BiocManager::install("package")
+source("https://bioconductor.org/biocLite.R")
+biocLite(suppressUpdates = FALSE)
+biocLite("phyloseq")
+biocLite("DESeq2")
+biocLite("ShortRead", suppressUpdates = FALSE)
 
-### install GitHub packages (tag = commit, branch or release tag)
-# install.packages("devtools")
-# devtools::install_github("user/repo", ref = "tag")
+
+install.packages("devtools")
+devtools::install_github("benjjneb/dada2")
+devtools::install_github("benjjneb/decontam")
+
+
+devtools::install_version("mvtnorm", version = "1.0-8", repos = "http://cran.us.r-project.org")
+devtools::install_version("fpc", version = "2.1-11.1", repos = "http://cran.us.r-project.org")
+biocLite("dendextend")
+
